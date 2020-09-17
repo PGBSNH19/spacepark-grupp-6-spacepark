4a81d74750d79bb13d2d2347188ea11e0023f297
Fredrik Odén <fredrikoden87@gmail.com>
2020-09-17 09:57:25 +0200

    Added reference for tests

---
+++ BackEnd/SpaceParkUnitTest/ParkingspotTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using spaceparkapi.Models;
+
+namespace SpaceParkUnitTest
+{
+    [TestClass]
+    public class ParkingSpotTests
+    {
+        // [TestMethod]
+        // public void ParkingspotModel_SpaceshipFits_False()
+        // {
+        //     bool spaceshipFits = Parkingspot.SpaceshipFits(99999999999);
+        //     Assert.IsFalse(spaceshipFits);
+        // }
+
+        // [TestMethod]
+        // public void ParkingspotModel_SpaceshipFits_True()
+        // {
+        //     bool spaceshipFits = Parkingspot.SpaceshipFits(1);
+        //     Assert.IsTrue(spaceshipFits);
+        // }
+    }
+}
