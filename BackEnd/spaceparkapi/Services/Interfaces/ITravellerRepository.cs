355e667eb665ae31e2400afee7eb1e933da682f8
Ahmad Yassin <31723305+akyassin@users.noreply.github.com>
2020-09-17 10:08:49 +0200

    Add traveller repo and irepo

---
+++ BackEnd/spaceparkapi/Services/Interfaces/ITravellerRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace spaceparkapi.Services.Interfaces
+{
+    public interface ITravellerRepository : IRepository
+    {
+    }
+}
