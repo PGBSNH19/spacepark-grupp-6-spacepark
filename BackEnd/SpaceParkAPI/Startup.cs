e94c1c52cfa08533afced06f2987f7d1428f8ad0
AndreMorad <56093049+AndreMorad@users.noreply.github.com>
2020-09-17 14:17:19 +0200

    Created MappedProfile file
    Created the file and the folder. Installed AutoMapper and added the automapper support in to starup file.

--- BackEnd/SpaceParkAPI/Startup.cs
+++ BackEnd/SpaceParkAPI/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -34,6 +35,10 @@ namespace spaceparkapi
             {
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddMvc(option => option.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            services.AddAutoMapper(typeof(Startup));
 
             services.AddScoped<IParkingspotRepository, ParkingspotRepository>();
             services.AddScoped<ISpaceportRepository, SpaceportRepository>();
