abd285a68b1d72914fe4215b2ca86ed0f3da557e
Nor Shiervani <50927017+NorShiervani@users.noreply.github.com>
2020-09-21 14:39:19 +0200

    Added GetAvailableParkinspot-method in Spaceport-repository.

--- BackEnd/SpaceParkAPI/Services/Repositories/SpaceportRepository.cs
+++ BackEnd/SpaceParkAPI/Services/Repositories/SpaceportRepository.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Extensions.Logging;
 using spaceparkapi.DBContext;
+using spaceparkapi.Models;
 using spaceparkapi.Services.Interfaces;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace spaceparkapi.Services.Repositories
 {
@@ -13,6 +16,16 @@ namespace spaceparkapi.Services.Repositories
         {
             _context = context;
             _logger = logger;
+        }
+
+        public async Task<Parkingspot> GetAvailableParkingspot(int spaceportId, Spaceship spaceship, params string[] including)
+        {
+            _logger.LogInformation($"Fetching available parkingspot from the spaceport.");
+
+            var spaceport = await Get<Spaceport>(spaceportId, including);
+
+            //'500' needs to be replaced with Fredriks' new SpaceshipFits()-method in Spaceship-model.
+            return spaceport.ParkingSpots.FirstOrDefault(x => x.ParkedSpaceship == null && spaceship.Length <= 500);
         }
     }
 }
