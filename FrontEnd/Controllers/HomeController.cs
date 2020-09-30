3c1337a19490eb90ea2faeac697c882041cfaaac
Ahmad Yassin <31723305+akyassin@users.noreply.github.com>
2020-09-30 14:12:22 +0200

    Creates HomeController Methods

--- FrontEnd/Controllers/HomeController.cs
+++ FrontEnd/Controllers/HomeController.cs
@@ -3,9 +3,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using SpaceparkWebApp.Models;
+using SpaceparkWebApp.Services;
 using System;
 using System.Diagnostics;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace SpaceparkWebApp.Controllers
@@ -14,11 +16,13 @@ namespace SpaceparkWebApp.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IConfiguration _configuration;
+        private readonly HelpServices _helpServices;
 
-        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration, HelpServices helpServices)
         {
             _logger = logger;
             _configuration = configuration;
+            _helpServices = helpServices;
         }
 
         public string Name { get; set; }
@@ -44,30 +48,53 @@ namespace SpaceparkWebApp.Controllers
             }
             catch (Exception)
             {
-                return View("Error");
+                TempData["msg"] = "API program is not running or the person is not from SpacPark database...!";
+                return RedirectToAction("Index");
             }
         }
 
-        public  IActionResult Create(string Name)
+        public async Task<IActionResult> Park(int id, string name)
         {
-            return View("Create");
+            HttpClient _client = new HttpClient();
+            Traveller travellerResult = _helpServices.GetTraveller(name).Result;
+
+            try
+            {
+                //Get the first free parking...
+                var spaceship = _helpServices.GetSpaceship(id);
+                var parkingUrl = _configuration["ApiHostUrl"] + "/api/v1.0/spaceport/getparkingspot/?spaceshipLength=" + (Convert.ToInt32(spaceship.Result.Length)).ToString();
+                string parkResponse = await _client.GetStringAsync(parkingUrl);
+                Parkingspot parking = JsonConvert.DeserializeObject<Parkingspot>(parkResponse);
+
+                //Park the spaceship in this parking...
+                Parkingspot parkingspot = new Parkingspot()
+                {
+                    SpaceportId = 500,
+                    ParkedSpaceshipId = id
+                };
+
+                var json = JsonConvert.SerializeObject(parkingspot);
+
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+
+                string url = _configuration["ApiHostUrl"] + "/api/v1.0/parkingspot/parkSpaceship/";
+                var response = await _client.PutAsync(url, content);
+
+
+                return View("Details", travellerResult);
+            }
+            catch (Exception)
+            {
+                TempData["msg"] = "There is something wrong...!";
+                return RedirectToAction("Details", travellerResult);
+            }
         }
 
-        public IActionResult Edit(string Name)
+        public async Task<IActionResult> Unpark(string Name)
         {
-            return View("Edit");
-        }
-
-        public IActionResult Delete(string Name)
-        {
-            return View("Details");
-        }
-
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var traveller = "";
+            return View("Details", traveller);
         }
     }
 }
