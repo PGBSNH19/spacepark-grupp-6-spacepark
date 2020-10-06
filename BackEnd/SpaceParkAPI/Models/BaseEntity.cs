806bb03852bf3ce503bdfbfaaa23f1f08fb6c209
Nor Shiervani <50927017+NorShiervani@users.noreply.github.com>
2020-10-06 22:04:41 +0200

    Added small comment about BaseEntity model.

--- BackEnd/SpaceParkAPI/Models/BaseEntity.cs
+++ BackEnd/SpaceParkAPI/Models/BaseEntity.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 namespace spaceparkapi.Models
 {
+    // Required for the generic method, getting entity by its Id.
     public class BaseEntity
     {
         public int Id { get; set; }
